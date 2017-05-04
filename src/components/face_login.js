877562aa1b1907948918147b727c2757e80068e3
Vita <vitac215@gmail.com>
2017-05-04 18:51:43 -0400

    before server test

--- src/components/face_login.js
+++ src/components/face_login.js
@@ -5,15 +5,47 @@ import * as actions from '../actions';
 
 class FaceLogin extends Component {
 
-  render() {
+  onSubmit() {
+    let username = localStorage.getItem('username');
+    console.log("file", this.props.file);
+    if (username !== null) {
+      let file = this.props.file;
+      this.props.faceLogin({ username, file });
+    } else {
+      alert("internal error!");
+    }
+  }
 
+  onImageChange(e) {
+    console.log(e);
+    e.preventDefault();
+    let reader = new FileReader();
+    let file = e.target.files[0];
+    this.props.uploadImg(file);
+  }
+
+  render() {
     return (
       <div>
         <div>Hello!</div>
         <div>You can login with your face now!</div>
-        {/* <img src={require('../../img/user.png')}></img> // png doesnt work? */}
-        <input type="file" accept="image/*" capture="camera" />
-        <button type="submit" className="btn btn-primary">Upload</button>
+        {/* <img src={require('img/user.png')}></img>  */}
+
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <input
+            type="file"
+            accept="image/*"
+            capture="camera"
+            onChange={this.onImageChange.bind(this)}
+          />
+          <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={this.onSubmit.bind(this)}>
+            Upload
+          </button>
+        </form>
+
       </div>
     )
   }
@@ -21,7 +53,10 @@ class FaceLogin extends Component {
 
 
 function mapStateToProps(state) {
-  return { errorMsg: state.auth.error };
+  return {
+    errorMsg: state.auth.error,
+    file: state.file.file,
+  };
 }
 
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,11 +3,13 @@ import { reducer as formReducer } from 'redux-form';
 
 import authReducer from './auth_reducer';
 import userTypeReducer from './user_type_reducer';
+import uploadImg from './upload_img_reducer';
 
 const rootReducer = combineReducers({
   form: formReducer,
   auth: authReducer,
-  user: userTypeReducer
+  user: userTypeReducer,
+  file: uploadImg
 });
 
 export default rootReducer;
