756d2cecb541f5d77e15bf604ff3d5938e38175e
Johnny Guo <guozj94@gmail.com>
2017-05-04 22:56:22 -0400

    dashboard

--- src/components/admin_dashboard.js
+++ src/components/admin_dashboard.js
@@ -9,9 +9,35 @@ export default class AdminDashboard extends Component {
 
     return (
       <div>
-        <img className="hamburger" src="/img/hamburger.svg" />
         <div className="nav">
-          <span className="title">FamilyPay</span>
+          FamilyPay
+        </div>
+        <img className="hamburger" src="/img/hamburger.svg" />
+        <div id="container-under">
+          <div style={{height: 20 + 'px'}}></div>
+          <div className="r16">PNC Checking (..9999)</div>
+          <div style={{height: 20 + 'px'}}></div>
+          <div className="a-member">
+            <div className="row">
+              <div className="col-xs-3 col-sm-3">
+                <img className="member-photo-md" src="" width="60px" height="60px" />
+              </div>
+              <div className="col-xs-9 col-sm-9">
+                <div style={{height: 5 + 'px'}}></div>
+                <div>
+                  <span>Noah</span>
+                  <span>&ensp;&ensp;&ensp;</span>
+                  <span className="r16d">1 Request</span>
+                </div>
+                <div style={{height: 18 + 'px'}}></div>
+                <div className="row">
+                  <div className="col-xs-2">0</div>
+                  <div className="col-xs-8"></div>
+                  <div className="col-xs-2">1000</div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
