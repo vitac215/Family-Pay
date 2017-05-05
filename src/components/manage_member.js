e4363a899fab95b666cca89c1a53d79d030d1691
Johnny Guo <guozj94@gmail.com>
2017-05-05 07:15:01 -0400

    heiheihei:

--- src/components/manage_member.js
+++ src/components/manage_member.js
@@ -17,7 +17,7 @@ export default class ManageMember extends Component {
         <img className="hamburger" src="/img/hamburger.svg" />
         <div style={{height: 20 + 'px'}}></div>
         <div style={{textAlign: 'center'}}>Noah</div>
-        <div id="container-under">
+        <div id="container-under" style={{marginTop: '10px'}}>
           <div className="row">
             <div className="col-xs-1 col-sm-1">
               <img src="/img/ring.svg" />
@@ -34,12 +34,14 @@ export default class ManageMember extends Component {
             <button>Decline</button>
           </div>
           <div style={{height: 30 + 'px'}}></div>
-          <div style={{'text-align': 'center'}}>Monthly Quota of This Member</div>
+          <div style={{marginLeft: '10%'}}>Monthly Quota of This Member</div>
           <div style={{height: 10 + 'px'}}></div>
           <input type="text" className="form-control myinput input-in-app" placeholder="Monthly Quota" autoComplete="off" />
-          <div style={{height: 52 + 'px'}}></div>
-          <div style={{'text-align': 'center'}}>Maximum One-Time Spending Limit</div>
+          <div style={{height: 42 + 'px'}}></div>
+          <div style={{marginLeft: '10%'}}>Max One-Time Spending Limit</div>
           <div style={{height: 10 + 'px'}}></div>
+          <input type="text" className="form-control myinput input-in-app" placeholder="Monthly Quota" autoComplete="off" />
+          <div style={{height: 40 + 'px'}}></div>
           <Link to="/main" className="primary-button">Done</Link>
 
         </div>
