00a612e22b6bfe31ff7c2f2db2b29cc26cf86514
Johnny Guo <guozj94@gmail.com>
2017-05-05 06:55:35 -0400

    main child

--- src/components/main_child.js
+++ src/components/main_child.js
@@ -10,25 +10,43 @@ class MainChild extends Component {
   render() {
     return (
       <div>
-
-        <div className="nav">
-          { localStorage.getItem('firstName') }
+        <div className="nav-big">
+          <div className="nav-title">Noah</div>
+          <img src="/img/default-user-avatar.svg" className="nav-big-photo" width="120px" height="120px" />
         </div>
+        <img className="hamburger" src="/img/hamburger.svg" />
+        <div style={{height: 20 + 'px'}}></div>
+        <div style={{textAlign: 'center'}}>Noah</div>
+        <div id="container-under" style={{marginTop: '20px'}}>
+          <div style={{textAlign: 'center'}}>You have spent 300 out of 1000</div>
+          <div style={{height: 20 + 'px'}}></div>
+          <div className="row">
+            <div className="col-xs-2 t16">0&ensp;&ensp;&ensp;</div>
+            <div className="col-xs-8 bar">
+              <div className="inner-bar r14w">$300</div>
+            </div>
+            <div className="col-xs-2 t16">1000</div>
+          </div>
+          <div style={{height: 20 + 'px'}}></div>
+          <div className="myrow two-button">
+            <button style={{width: '120px'}}>Make Payment</button>
+            <div style={{width: 15 + '%'}}></div>
+            <button style={{width: '120px'}}>Send Request</button>
+          </div>
+          <div style={{height: 20 + 'px', borderBottom: '1px solid #e6e6e6'}}></div>
+          <div style={{height: 20 + 'px'}}></div>
+          <div className="personal-transaction-container" style={{paddingLeft: '20px', paddingRight: '20px'}}>
+            <div>You paid Jacob $20</div>
+            <div className="transaction-record-text-date">5 days ago</div>
+            <div style={{height: 10 + 'px'}}></div>
+            <div>You spent $20 on Amazon</div>
+            <div className="transaction-record-text-date">8 days ago</div>
+            <div style={{height: 10 + 'px'}}></div>
+            <div>You spent $10 at Giant Eagle</div>
+            <div className="transaction-record-text-date">10 days ago</div>
+          </div>
 
-        <img className="user-avatar img-circle" src={this.props.file} />
-
-        <div>
-          {/* <p>You've spent ${this.props.spent} out of ${this.props.limit}</p> */}
-          <p>You've spent $300 out of $1000</p>
         </div>
-
-        <button type="submit" className="primary-button">
-          Make Payment
-        </button>
-        <button type="submit" className="primary-button">
-          Send Request
-        </button>
-
       </div>
     )
   }; // end render
--- src/index.js
+++ src/index.js
@@ -20,6 +20,7 @@ import AdminDashboard from './components/admin_dashboard';
 import AddMember from './components/add_member';
 import ManageMember from './components/manage_member';
 import ViewDetail from './components/view_detail';
+import MainChild from './components/main_child';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
 const store = createStoreWithMiddleware(reducers);
@@ -45,6 +46,7 @@ ReactDOM.render(
         <Route path="/add_member" component={AddMember} />
         <Route path="/manage_member" component={ManageMember} />
         <Route path="/view_detail" component={ViewDetail} />
+        <Route path="/main_child" component={MainChild} />
       </Route>
     </Router>
   </Provider>
